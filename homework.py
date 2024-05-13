2dfa110d88553282d95edd41589357a868163c06
Stepan Gerasimov <mr.gerasimov.stepan@yandex.ru>
2024-05-13 22:38:51 +0300

    second

--- homework.py
+++ homework.py
@@ -1,12 +1,14 @@
 import os
-from dotenv import load_dotenv
-import requests
 import time
 import logging
 from http import HTTPStatus
 
+from dotenv import load_dotenv
+import requests
 from telebot import TeleBot
+
 import exceptions
+
 load_dotenv()
 
 
@@ -17,8 +19,6 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-TIME = {'from_date': 1549962000}
-
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -27,17 +27,24 @@ HOMEWORK_VERDICTS = {
 }
 
 
-def check_tokens():
+def check_tokens() -> bool:
     """есть ли токены в переменных окружения."""
-    if (PRACTICUM_TOKEN is not None and TELEGRAM_TOKEN is not None
-            and TELEGRAM_CHAT_ID is not None):
+    no_token = ''
+
+    if PRACTICUM_TOKEN is None:
+        no_token += 'PRACTICUM_TOKEN '
+    if TELEGRAM_TOKEN is None:
+        no_token += 'TELEGRAM_TOKEN '
+    if TELEGRAM_CHAT_ID is None:
+        no_token += 'TELEGRAM_CHAT_ID'
+    if no_token == '':
         return True
     else:
-        logging.critical('no_token')
+        logging.critical(no_token)
         raise exceptions.NoTokensException()
 
 
-def send_message(bot, message):
+def send_message(bot: TeleBot, message: str) -> bool:
     """Отправляет сообщение о статусе ревью."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
@@ -49,7 +56,7 @@ def send_message(bot, message):
         return True
 
 
-def get_api_answer(timestamp):
+def get_api_answer(timestamp: time) -> dict:
     """Запрос к api."""
     try:
         homework_statuses = requests.get(
@@ -64,24 +71,24 @@ def get_api_answer(timestamp):
         logging.error('endpoint_error')
 
 
-def check_response(response):
+def check_response(response: dict):
     """Проверка ответа API."""
     if not isinstance(response, dict):
         raise TypeError()
-    elif 'homeworks' not in response.keys():
+    if 'homeworks' not in response:
         raise exceptions.NoKeyHmwrkException()
-    elif not isinstance(response['homeworks'], list):
+    if not isinstance(response['homeworks'], list):
         raise TypeError()
-    elif len(response['homeworks']) == 0:
+    if len(response['homeworks']) == 0:
         logging.debug('no homework')
 
 
-def parse_status(homework):
+def parse_status(homework: dict) -> str:
     """Извлекает информацию о конкретной работе."""
-    if 'homework_name' not in homework.keys():
+    if 'homework_name' not in homework:
         raise exceptions.NoKeyHmwrkNameException()
-    elif ('status' not in homework.keys()
-            or homework['status'] not in HOMEWORK_VERDICTS.keys()):
+    if ('status' not in homework.keys()
+            or homework['status'] not in HOMEWORK_VERDICTS):
         raise exceptions.StatusException()
 
     return (f'Изменился статус проверки работы "{homework["homework_name"]}"'
@@ -90,12 +97,12 @@ def parse_status(homework):
 
 def main():
     """Main для бота."""
+    check_tokens()
     bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     status = None
     while True:
         try:
-            check_tokens()
             response = get_api_answer({'from_date': timestamp})
             check_response(response)
             new_status = parse_status(response['homeworks'][0])
